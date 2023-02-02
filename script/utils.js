0bf2b249b4e9db1ad66ddf996016ea8313e700ab
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-02-02 14:53:25 +0100

    fixat till funktionen som skapar divar för varje bokstav

--- script/utils.js
+++ script/utils.js
@@ -1,11 +1,20 @@
 import wordList from "../JSON/word-list.json" assert { type: "json" };
 
+const letterDivContainer = document.querySelector('.letter-div-container')
 
 export default function generateRandomWord() {
 	let word = wordList[Math.floor(Math.random() * wordList.length)];
-	console.log(word);
-	return word;
+
+	for (let i = 0; i < word.length; i++) {
+		const letter = word[i];
+		const letterDiv = document.createElement('div')
+		letterDiv.innerText = letter
+		letterDivContainer.append(letterDiv)
+	}
 }
+
+
+
 
 
 
