cc755cb00f3ef73716a02edb78abeeaff97c3948
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-02-02 15:56:19 +0100

    påbörjat funktion som ska jämföra bokstaven man skriver in med bokstäverna i ordet, inte klar än

--- script/utils.js
+++ script/utils.js
@@ -1,18 +1,50 @@
 import wordList from "../JSON/word-list.json" assert { type: "json" };
 
 const letterDivContainer = document.querySelector('.letter-div-container')
+const letterInput = document.querySelector('#letter-input')
+const wrongLetterContainer = document.querySelector('.wrong-letter-container')
+
+let letter = ''
+let letterDiv = null
+let word = ''
 
 export default function generateRandomWord() {
-	let word = wordList[Math.floor(Math.random() * wordList.length)];
+	word = wordList[Math.floor(Math.random() * wordList.length)];
+
+	console.log(word);
 
 	for (let i = 0; i < word.length; i++) {
-		const letter = word[i];
-		const letterDiv = document.createElement('div')
-		letterDiv.innerText = letter
+		letter = word[i];
+		letterDiv = document.createElement('div')
+
 		letterDivContainer.append(letterDiv)
 	}
 }
 
+letterInput.addEventListener('keydown', event => {
+	if (event.key == 'Enter') {
+		compareLetters()
+		letterInput.value = ''
+	}
+})
+
+console.log(letterInput.value);
+
+function compareLetters() {
+
+	for (let l = 0; l < word.length; l++) {
+
+		if (letterInput.value === letter) {
+			letterDiv.innerText = letter
+			console.log('right!');
+		}
+		else {
+			const wrongLetter = document.createElement('p')
+			wrongLetter.innerText = letter
+			console.log('try again')
+		}
+	}
+}
 
 
 
